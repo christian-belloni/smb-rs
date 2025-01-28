17184391789e964eb83ca4f0ee160e3af6e71810
Aviv Naaman <aviv@captain-eye.com>
2025-01-28 07:51:59 +0200

    split to lib & cli crates.

--- smb-cli/src/main.rs
+++ smb-cli/src/main.rs
@@ -1,12 +1,12 @@
 use clap::{Parser, Subcommand};
-use smb::{
+use smb_lib::{
     client::Client,
     packets::smb2::{create::CreateDisposition, fscc::FileAccessMask},
     resource::Resource,
     session::Session,
     tree::Tree,
 };
-use std::{error::Error, io::prelude::*, str::FromStr};
+use std::{error::Error, str::FromStr};
 
 #[derive(Debug, Clone)]
 struct UncPath {
@@ -124,58 +124,6 @@ fn main() -> Result<(), Box<dyn Error>> {
         }
     }
 
-    // let mut smb = Client::new();
-    // smb.connect("3.70.138.47:445")?;
-    // smb.negotiate()?;
-    // let mut session = smb.authenticate("LocalAdmin".to_string(), "123456".to_string())?;
-    // let mut tree = session.tree_connect(r"\\AVIVVM\MyShare".to_string())?;
-    // let file = tree.create(
-    //     r"ntoskrnl.exe".to_string(),
-    //     CreateDisposition::Open,
-    //     FileAccessMask::new()
-    //         .with_generic_read(true)
-    //         .with_generic_write(false),
-    // )?;
-    // match file {
-    //     smb::resource::Resource::File(mut smbfile) => {
-    //         log::info!(
-    //             "File created {}, modified {}",
-    //             smbfile.handle.created(),
-    //             smbfile.handle.modified()
-    //         );
-
-    //         // Begin by querying more information about this file.
-    //         let info = smbfile.query_info()?;
-    //         log::info!("File info: {:?}", info);
-
-    //         // Let's read some data from the file.
-    //         let mut buf = [0; 0x1000];
-    //         let n = smbfile.read(&mut buf)?;
-    //         println!("{:?}", String::from_utf8_lossy(&buf[..n]));
-
-    //         // Let's write some data to the file.
-    //         // smbfile.write_all(b"Hello, world!")?;
-    //     }
-    //     smb::resource::Resource::Directory(mut smbdirectory) => {
-    //         log::info!(
-    //             "Directory created {}, modified {}",
-    //             smbdirectory.handle.created(),
-    //             smbdirectory.handle.modified()
-    //         );
-
-    //         for (i, item) in smbdirectory.query("*")?.iter().enumerate() {
-    //             log::info!(
-    //                 "{i}|{} {}",
-    //                 if item.file_attributes.directory() {
-    //                     "d"
-    //                 } else {
-    //                     "f"
-    //                 },
-    //                 item.file_name,
-    //             );
-    //         }
-    //     }
-    // }
     Ok(())
 }
 
