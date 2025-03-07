9e0cb4a40d490ee120aa4aa0cae45cd4c8b759db
Aviv Naaman <aviv@captain-eye.com>
2025-03-07 16:42:03 +0200

    CI: change the endpoint from localhost to the IP of the running samba service

--- smb/tests/integration_tests.rs
+++ smb/tests/integration_tests.rs
@@ -1,25 +1,41 @@
+use log::info;
 use smb::{
     packets::smb2::{CreateDisposition, FileAccessMask},
     Connection,
 };
 use std::env::var;
 
+fn init_logger() {
+    env_logger::builder()
+        .is_test(true)
+        .filter_level(log::LevelFilter::Trace)
+        .init();
+}
+
 #[maybe_async::test(
     feature = "sync",
     async(feature = "async", tokio::test(flavor = "multi_thread"))
 )]
 pub async fn test_smb_integration_basic() -> Result<(), Box<dyn std::error::Error>> {
+    init_logger();
+
     use smb::packets::smb2::FileDispositionInformation;
 
     let mut smb = Connection::new();
     // Default to localhost, LocalAdmin, 123456
-    let server = var("SMB_SERVER").unwrap_or("127.0.0.1:445".to_string());
-    let user = var("SMB_USER").unwrap_or("LocalAdmin".to_string());
-    let password = var("SMB_PASSWORD").unwrap_or("123456".to_string());
+    let server = var("SMB_RUST_TESTS_SERVER").unwrap_or("127.0.0.1:445".to_string());
+    let user = var("SMB_RUST_TESTS_USER_NAME").unwrap_or("LocalAdmin".to_string());
+    let password = var("SMB_RUST_TESTS_PASSWORD").unwrap_or("123456".to_string());
+
+    info!("Connecting to {} as {}", server, user);
+
     // Connect & Authenticate
     smb.connect(&server).await?;
+    info!("Connected, authenticating...");
     let mut session = smb.authenticate(&user, password).await?;
+    info!("Authenticated!");
     let mut tree = session.tree_connect("MyShare").await?;
+    info!("Connected to share, start test basic");
 
     // Hello, World! > test.txt
     {
--- smb/tests/start.sh
+++
@@ -1,7 +0,0 @@
-
-
-docker run --name smb-test --rm --network bridge -p 445:445 --cap-add NET_ADMIN  \
-    -e  \
-    smb-tests
-
-# Shows logs in background and 
